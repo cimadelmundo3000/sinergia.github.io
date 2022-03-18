5eea759498602481c70486d8cff54c7d248a6f36
cimadelmundo3000 <cimadelmundo3000@gmail.com>
2022-03-18 12:34:18 -0400

    update 18-03 segundo

---
+++ contact-ssd.php
@@ -0,0 +1,18 @@
+<?php
+/*if(isset($_POST['send']) && !empty($_POST['send'])){
+
+    $name = addslashes($_POST['name']);
+    $city = addslashes($_POST['city']);
+    $email = addslashes($_POST['email']);
+    $phone = addslashes($_POST['phone']);
+    $subject = addslashes($_POST['subject']);
+    $how = addslashes($_POST['how']);
+    $message = addslashes($_POST['message']);
+*/
+    echo "const vehiculo = {
+    marca: "Mitsubishi",
+    modelo: "Lancer GLX",
+};";
+
+//}
+?>
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+// MENU
+//////////////////////////////
 $(window).scroll(function() {
     let s = $(window).scrollTop();
     if(s > 70){
@@ -7,21 +9,30 @@ $(window).scroll(function() {
     }
 });
 
+// IR A SECCION 2
+//////////////////////////////
 $('body .seccion1').delegate('#irseccion2', 'click',function(){
     $("html, body").animate({ scrollTop: $('#seccion2').offset().top }, 800);
     return false;
 });
 
+// CTA DE "ME INTERESA"
+//////////////////////////////
 $('body').delegate('.cta', 'click',function(){
     $("html, body").animate({ scrollTop: $('#contacto').offset().top }, 1200);
     return false;
 });
 
+// SCROLL TO TOP
+//////////////////////////////
 $('body .header').delegate('.btn, .mainlogo', 'click',function(){
     let t = $(this).data('target');
     $("html, body").animate({ scrollTop: $('#'+t).offset().top }, 700);
     return false;
 });
+
+// OPEN MENU MOVIL
+//////////////////////////////
 $('body .header').delegate('.menuMovil','click',function(event){
     if(!$(this).hasClass('active')){
         $(this).addClass(('active'));
@@ -33,9 +44,31 @@ $('body .header').delegate('.menuMovil','click',function(event){
     event.preventDefault();
     return false;
 });
-// alert(window.innerWidth+'-'+window.innerHeight)
+
+// CLOSE MENU MOVIL
+//////////////////////////////
 $('body .header').delegate('.close, .btn','click',function(event){
         $('body .header .menuMovil').removeClass(('active'));
         $('body .header .btns:eq(0)').removeClass('active');
     return false;
+});
+
+// ENVIO DE FORMULARIO
+//////////////////////////////
+$("body").delegate('#form-contacto','submit',function(event){
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    $.ajax({
+        type: "POST",
+        url: "https://tiendasnyco.com/contact-ssd.php",
+        crossDomain: true,
+        data: {
+            d: 1
+        },
+    }).done(function (data) {
+        console.log(data);
+    }); 
+
 });
